f6c3a21317c165ab1f698426552f66ad4065897b
Renner Gábor <grenner@intrend.hu>
2017-04-13 19:15:11 +0200

    docker build

--- run-test.sh
+++ run-test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -ev
+set -e
 
 if [ "${VIRGO_VERSION}" = "3.7.0" ]; then
 	TEST_URL=https://localhost:8443/
@@ -9,3 +9,10 @@ else
 fi
 
 echo $TEST_URL
+# wait until Virgo is available and test with Splashscreen
+for i in 1 2 3
+do
+  echo "Trying $TEST_URL for $i. time..."
+  wget --retry-connrefused --waitretry=3 --read-timeout=30 --timeout=15 -t 10 -O /dev/null $TEST_URL && break || sleep 10; 
+done
+
